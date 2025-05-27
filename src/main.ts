fbbb6c9c769eea22bba6772d2a2153671e27fa1f
annakhizhniak <kukurika.anna@gmail.com>
2025-05-27 17:07:49 +0300

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/shared/assets/css/tailwind.css';
 
-import App from './App.vue'
-import router from './app/router'
+import App from './App.vue';
+import router from './app/router';
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+  .use(router)
+  .mount('#app')
