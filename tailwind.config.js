b43cca330c1d05a4514f5c1ae7ea42da93437e60
annakhizhniak <kukurika.anna@gmail.com>
2025-05-28 13:11:12 +0300

    [home] added main app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        'green-dark': 'rgba(42, 65, 48, 1)',
+        'green-dark': '#90d159',
+        'green-medium': '#99cd6a',
+        'green-light': '#b6e094',
+        'brown-dark': '#523629',
+        'brown-medium': '#735c52',
+        'brown-light': '#bdb6b0',
+        'gray-lighter': '#9d938c',
       },
     },
   },
