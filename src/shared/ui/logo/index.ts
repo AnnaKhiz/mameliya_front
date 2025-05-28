9960cfb09e27a5026b04cf5160d7bbd10f6d86a0
annakhizhniak <kukurika.anna@gmail.com>
2025-05-28 13:11:33 +0300

    [home] created logo component

---
+++ src/shared/ui/logo/index.ts
@@ -0,0 +1,3 @@
+import LogoMain from '@/shared/ui/logo/ui/LogoMain.vue';
+
+export default LogoMain;
---
+++ src/shared/ui/logo/ui/LogoMain.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import logoImage from '@/shared/assets/images/logo_img.png';
+import logoText from '@/shared/assets/images/logo_text.png';
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <img
+      :src="logoImage"
+      alt="logo image"
+      class="w-20 h-full object-contain"
+    >
+    <img
+      :src="logoText"
+      alt="logo text"
+      class="h-16
+      object-contain"
+    >
+  </div>
+</template>
