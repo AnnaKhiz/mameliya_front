aaa48f2e2e130872e76ebd2fa10cddcf9598471b
annakhizhniak <kukurika.anna@gmail.com>
2025-05-28 15:11:48 +0300

    [home] added footer contacts block component

---
+++ src/shared/ui/contacts/index.ts
@@ -0,0 +1,3 @@
+import ContactsFooter from '@/shared/ui/contacts/ui/ContactsFooter.vue';
+
+export default ContactsFooter;
---
+++ src/shared/ui/contacts/ui/ContactsFooter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 text-brown-dark">
+    <p class="font-semibold uppercase">Contacts</p>
+    <div>
+      <span class="font-semibold">Email: </span>
+      <a href="mailto:email@gmail.com" class="hover:text-brown-light transition duration-500">
+        <span>email@gmail.com</span>
+      </a>
+    </div>
+    <div>
+      <span class="font-semibold">Phone: </span>
+      <a href="tel:+407001111111" class="hover:text-brown-light transition duration-500">
+        <span>+407201111111</span>
+      </a>
+    </div>
+  </div>
+</template>
