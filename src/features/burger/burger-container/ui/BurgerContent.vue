77f92535f0276fc07a5c7a447fb0d78f4f28723b
annakhizhniak <kukurika.anna@gmail.com>
2025-05-28 20:02:25 +0300

    [home] fixed key value

--- src/features/burger/burger-container/ui/BurgerContent.vue
+++ src/features/burger/burger-container/ui/BurgerContent.vue
@@ -17,7 +17,7 @@ const burgerMenuList = ref([
   <div class="absolute inset-y-0 w-[50%] bg-brown-dark px-3 py-5" >
     <p
       v-for="item in burgerMenuList"
-      :key="item"
+      :key="item.value"
       class="p-2.5 text-center bg-brown-light mb-2 cursor-pointer hover:bg-brown-medium transition duration-500 rounded hover:text-white"
     >
       {{ item.text }}</p>
