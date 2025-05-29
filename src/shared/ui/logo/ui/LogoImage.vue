008290962832b8c1755221cae40578768beede53
annakhizhniak <kukurika.anna@gmail.com>
2025-05-29 17:14:17 +0300

    [home] added links to logo images

--- src/shared/ui/logo/ui/LogoImage.vue
+++ src/shared/ui/logo/ui/LogoImage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import logoImage from '@/shared/assets/images/logo_img.png'
+import logoImage from '@/shared/assets/images/logo_img.png';
+import { useRouter } from 'vue-router'
+const router = useRouter();
 </script>
 
 <template>
-  <div class="w-20 h-20 cursor-pointer">
+  <div class="w-20 h-20 cursor-pointer" @click="router.push({ name: 'home'})">
     <img :src="logoImage" alt="logo text" class="h-full w-auto object-cover m-auto">
   </div>
 </template>
--- src/shared/ui/logo/ui/LogoText.vue
+++ src/shared/ui/logo/ui/LogoText.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import logoText from '@/shared/assets/images/logo_text.png';
+import { useRouter } from 'vue-router'
+const router = useRouter();
 </script>
 
 <template>
-  <div class="h-10 w-auto w-full mb-4">
+  <div class="h-10 w-auto w-full mb-4" @click="router.push({ name: 'home'})">
     <img :src="logoText" alt="logo text" class=" h-full w-auto object-cover">
   </div>
 </template>
