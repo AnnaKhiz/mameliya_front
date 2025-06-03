2126aa035ea7526712e402b3c4ba433b43efd267
annakhizhniak <kukurika.anna@gmail.com>
2025-06-03 15:55:26 +0300

    [front] added check is user auth on app mounted

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import {computed, ref, watchEffect} from 'vue';
+import {computed, onMounted, ref, watchEffect} from 'vue';
 import { RouterView } from 'vue-router';
 import { useRoute } from 'vue-router'
 import FooterComponent from '@/shared/ui/footer'
 import { MainHeader, UserHeader } from '@/shared/ui/header';
 const currentRoute = ref<string>();
 const route = useRoute();
+import { useUserStore } from "@/entities/user";
+import {storeToRefs} from "pinia";
+const { user } = storeToRefs(useUserStore());
+const { checkUserSession } = useUserStore();
 
 watchEffect(() => {
   currentRoute.value = route.fullPath;
 })
 
-const isAuthenticated = computed(() => (currentRoute.value?.includes('user')))
+const isAuthenticated = computed(() => (currentRoute.value?.includes('user')));
+
+onMounted(async () => {
+  await checkUserSession();
+})
 </script>
 
 <template>
