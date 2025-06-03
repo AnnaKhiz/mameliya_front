4460bc600036705f18f0a14716f0419d3c78e99d
annakhizhniak <kukurika.anna@gmail.com>
2025-06-03 17:18:35 +0300

    [front] updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <script setup lang="ts">
-import {computed, onMounted, ref, watchEffect} from 'vue';
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
-import { useRoute } from 'vue-router'
 import FooterComponent from '@/shared/ui/footer'
 import { MainHeader, UserHeader } from '@/shared/ui/header';
-const currentRoute = ref<string>();
-const route = useRoute();
 import { useUserStore } from "@/entities/user";
 import {storeToRefs} from "pinia";
-const { user } = storeToRefs(useUserStore());
+const { isAuthenticated } = storeToRefs(useUserStore());
 const { checkUserSession } = useUserStore();
-
-watchEffect(() => {
-  currentRoute.value = route.fullPath;
-})
-
-const isAuthenticated = computed(() => (currentRoute.value?.includes('user')));
 
 onMounted(async () => {
   await checkUserSession();
 })
+
 </script>
 
 <template>
