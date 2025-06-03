dc45da42f5fd4f403abcb40adf4c72af0d7d5c3b
annakhizhniak <kukurika.anna@gmail.com>
2025-06-03 20:56:56 +0300

    [front] updated path for logo link

--- src/shared/ui/logo/ui/LogoMain.vue
+++ src/shared/ui/logo/ui/LogoMain.vue
@@ -2,13 +2,23 @@
 import logoImage from '@/shared/assets/images/logo_img.png';
 import logoText from '@/shared/assets/images/logo_text.png';
 import { useRouter } from 'vue-router'
+import {storeToRefs} from "pinia";
+import {useUserStore} from "@/entities/user";
 const router = useRouter();
+const { isAuthenticated, user } = storeToRefs(useUserStore());
+const goToHomePage = () => {
+  if (isAuthenticated.value) {
+    router.push({ name: 'user-home', params: { id: user?.value?.userId }})
+  } else {
+    router.push({ name: 'home'});
+  }
+}
 </script>
 
 <template>
   <div
     class="flex items-center gap-2 cursor-pointer"
-    @click="router.push({ name: 'home'})"
+    @click="goToHomePage"
   >
     <img
       :src="logoImage"
