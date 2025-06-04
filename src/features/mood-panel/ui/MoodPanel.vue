a5dc071dc319e6867cbfc9f867ffda8581bb0875
annakhizhniak <kukurika.anna@gmail.com>
2025-06-04 19:48:13 +0300

    [front] changed mood states

--- src/features/mood-panel/ui/MoodPanel.vue
+++ src/features/mood-panel/ui/MoodPanel.vue
@@ -18,18 +18,18 @@ const openMoodPanel = () => {
 <template>
   <div class="relative">
     <FaceSmileIcon v-if="moodState === 'good'" class="fill-green-mood w-7 cursor-pointer" @click="openMoodPanel" />
-    <FaceSmileIcon v-if="moodState === 'bad'" class="fill-yellow-600 w-7 cursor-pointer" @click="openMoodPanel" />
-    <FaceFrownIcon v-if="moodState === 'normal'" class="w-7 cursor-pointer fill-red-900" @click="openMoodPanel" />
+    <FaceSmileIcon v-if="moodState === 'normal'" class="fill-yellow-600 w-7 cursor-pointer" @click="openMoodPanel" />
+    <FaceFrownIcon v-if="moodState === 'bad'" class="w-7 cursor-pointer fill-red-900" @click="openMoodPanel" />
     <Transition>
       <div v-if="isMoodPanel" class="flex items-center justify-center gap-2 absolute -top-12 left-0 bg-white/50 p-1 rounded-md shadow-2xl">
         <div class="hover:bg-white p-1 rounded-md transition duration-500 ">
           <FaceSmileIcon class="fill-green-mood w-7 cursor-pointer " @click="toggleMood('good')" />
         </div>
         <div class="hover:bg-white p-1 rounded-md transition duration-500">
-          <FaceSmileIcon class="fill-yellow-600 w-7 cursor-pointer" @click="toggleMood('bad')" />
+          <FaceSmileIcon class="fill-yellow-600 w-7 cursor-pointer" @click="toggleMood('normal')" />
         </div>
         <div class="hover:bg-white p-1 rounded-md transition duration-500">
-          <FaceFrownIcon class="w-7 cursor-pointer fill-red-900" @click="toggleMood('normal')" />
+          <FaceFrownIcon class="w-7 cursor-pointer fill-red-900" @click="toggleMood('bad')" />
         </div>
       </div>
     </Transition>
