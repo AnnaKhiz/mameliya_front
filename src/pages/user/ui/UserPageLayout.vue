8a88868c616ccb0980106fd72031d941af5019bc
annakhizhniak <kukurika.anna@gmail.com>
2025-06-06 13:16:12 +0300

    [front] aside menu. added translations for all aside info

--- src/pages/user/ui/UserPageLayout.vue
+++ src/pages/user/ui/UserPageLayout.vue
@@ -1,40 +1,42 @@
 <script setup lang="ts">
 import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 
 const asideList = computed(() => ([
   {
     id: 1,
-    title: 'Mama',
+    title: t('aside.mama.title'),
     value: 'mama',
     url: () => { console.log( 'mama')},
     links: [
       {
         id: 1,
-        title: 'Self-feeling',
+        title: t('aside.mama.self_feeling'),
         value: 'self_feeling',
         url: () => { console.log('Self-feeling')}
       },
       {
         id: 2,
-        title: 'Daily rituals',
+        title: t('aside.mama.daily_rituals'),
         value: 'rituals',
         url: () => { console.log('daily rituals')}
       },
       {
         id: 3,
-        title: 'Personal notes',
+        title: t('aside.mama.personal_notes'),
         value: 'personal_notes',
         url: () => { console.log('Notes')}
       },
       {
         id: 4,
-        title: '5 min timer',
+        title: t('aside.mama.timer'),
         value: 'timer',
         url: () => { console.log('5 min timer')}
       },
       {
         id: 5,
-        title: 'Calendar beauty procedures',
+        title:  t('aside.mama.beauty_calendar'),
         value: 'beauty_calendar',
         url: () => { console.log('calendar beauty procedures')}
       },
@@ -42,37 +44,37 @@ const asideList = computed(() => ([
   },
   {
     id: 2,
-    title: 'Family',
+    title: t('aside.family.title'),
     value: 'family',
     url: () => { console.log( 'family')},
     links: [
       {
         id: 1,
-        title: 'Family events',
+        title: t('aside.family.family_events'),
         value: 'events',
         url: () => { console.log('Events')}
       },
       {
         id: 2,
-        title: 'Weekend/weekly plans',
+        title: t('aside.family.plans'),
         value: 'plans',
         url: () => { console.log('Plans')}
       },
       {
         id: 3,
-        title: 'Weekly/daily menu plans',
+        title: t('aside.family.menu'),
         value: 'menu',
         url: () => { console.log('Menu')}
       },
       {
         id: 4,
-        title: 'Household chores, distribution of chores',
+        title: t('aside.family.chores'),
         value: 'chores',
         url: () => { console.log('Chores')}
       },
       {
         id: 5,
-        title: 'General calendar',
+        title: t('aside.family.general_calendar'),
         value: 'general_calendar',
         url: () => { console.log('General calendar')}
       },
@@ -80,37 +82,37 @@ const asideList = computed(() => ([
   },
   {
     id: 3,
-    title: 'Children',
+    title: t('aside.children.title'),
     value: 'children',
     url: () => { console.log( 'children')},
     links: [
       {
         id: 1,
-        title: 'Daily routine, schedule',
+        title: t('aside.children.daily_routine'),
         value: 'daily_routine',
         url: () => { console.log('Daily routine')}
       },
       {
         id: 2,
-        title: 'Activities',
+        title: t('aside.children.activities'),
         value: 'activities',
         url: () => { console.log('Activities')}
       },
       {
         id: 3,
-        title: 'health, medicaments, vitamins',
+        title: t('aside.children.health'),
         value: 'health',
         url: () => { console.log('Health')}
       },
       {
         id: 4,
-        title: 'Baby diary: weight, height',
+        title: t('aside.children.baby_diary'),
         value: 'baby_diary',
         url: () => { console.log('Baby diary')}
       },
       {
         id: 5,
-        title: 'Notes',
+        title: t('aside.children.children_notes'),
         value: 'children_notes',
         url: () => { console.log('Notes children')}
       },
@@ -118,37 +120,37 @@ const asideList = computed(() => ([
   },
   {
     id: 4,
-    title: 'Daily routine',
+    title: t('aside.daily_routine.title'),
     value: 'daily_routine',
     url: () => { console.log( 'Daily routine')},
     links: [
       {
         id: 1,
-        title: 'Daily tasks',
+        title: t('aside.daily_routine.daily_tasks'),
         value: 'daily_tasks',
         url: () => { console.log('Daily tasks')}
       },
       {
         id: 2,
-        title: 'Weekly tasks',
+        title: t('aside.daily_routine.weekly_tasks'),
         value: 'weekly_tasks',
         url: () => { console.log('Weekly tasks')}
       },
       {
         id: 3,
-        title: 'Recurring tasks',
+        title: t('aside.daily_routine.recurring_tasks'),
         value: 'recurring_tasks',
         url: () => { console.log('recurring tasks')}
       },
       {
         id: 4,
-        title: 'Priority tasks',
+        title: t('aside.daily_routine.priority_tasks'),
         value: 'priority_tasks',
         url: () => { console.log('Priority tasks')}
       },
       {
         id: 5,
-        title: 'Deferred tasks',
+        title: t('aside.daily_routine.deferred_tasks'),
         value: 'deferred_tasks',
         url: () => { console.log('Deferred tasks')}
       },
@@ -156,31 +158,31 @@ const asideList = computed(() => ([
   },
   {
     id: 5,
-    title: 'Mental health',
+    title: t('aside.mental_health.title'),
     value: 'mental_health',
     url: () => { console.log( 'Mental health')},
     links: [
       {
         id: 1,
-        title: 'Diary',
+        title: t('aside.mental_health.diary'),
         value: 'diary',
         url: () => { console.log('Diary')}
       },
       {
         id: 2,
-        title: 'Meditations',
+        title: t('aside.mental_health.meditations'),
         value: 'meditations',
         url: () => { console.log('Meditations')}
       },
       {
         id: 3,
-        title: 'AI assistant',
+        title: t('aside.mental_health.assistant'),
         value: 'assistant',
         url: () => { console.log('Assistant')}
       },
       {
         id: 4,
-        title: 'Favorite books',
+        title: t('aside.mental_health.favorite_books'),
         value: 'favorite_books',
         url: () => { console.log('Favorite books')}
       }
