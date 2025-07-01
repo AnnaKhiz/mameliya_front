f5f09841bcd1d23f00bbd0b46c35097c760a9838
annakhizhniak <kukurika.anna@gmail.com>
2025-07-01 19:29:26 +0300

    [front] added separate component AddEditEventForm

--- src/entities/user/model/useUserStore.ts
+++ src/entities/user/model/useUserStore.ts
@@ -7,7 +7,7 @@ import type {FormFieldsType, UserDataType, ResponseType, CalendarEventType} from
 export const useUserStore = defineStore('user', () => {
   const user = ref<UserDataType | null>(null);
   const isAuthenticated = ref<boolean>(false);
-  const userCalendarEvents = ref<Record<string, any> | null>(null);
+  const userCalendarEvents = ref<CalendarEventType[] | null>(null);
   const isLoading = ref<boolean>(false);
 
   const updateIsAuthenticated = (value: boolean) => {
@@ -91,7 +91,9 @@ export const useUserStore = defineStore('user', () => {
     try {
       result = await fetchData('user/google/events/:type', 'GET', { type });
       isLoading.value = false;
-      userCalendarEvents.value = result?.data?.events;
+      userCalendarEvents.value = parseUserCalendarEvents(result?.data?.events);
+      console.log('userCalendarEvents.value', userCalendarEvents.value)
+      // userCalendarEvents.value = result?.data?.events;
     } catch(error) {
       console.error('Error [CAL EVENTS]: ', error);
     }
@@ -105,7 +107,10 @@ export const useUserStore = defineStore('user', () => {
       result = await fetchData('user/google/event/add/:type', 'POST', { type }, body);
       console.log('[ADD EVENT] result', result)
       isLoading.value = false;
-      userCalendarEvents.value?.push(result?.data)
+      const resultArray = [];
+      resultArray.push(result?.data)
+      const parsedEvent: CalendarEventType[] = parseUserCalendarEvents(resultArray);
+      userCalendarEvents.value?.push(...parsedEvent)
     } catch(error) {
       console.error('Error [ADD EVENT TO CAL]: ', error);
     }
@@ -127,6 +132,32 @@ export const useUserStore = defineStore('user', () => {
     return result;
   }
 
+  const parseUserCalendarEvents = (result: CalendarEventType[]) => {
+    console.log('result', result)
+
+
+    return result.map((e: Record<string, any>) => {
+
+      return {
+        start: new Date(e.start.dateTime || e.start.date),
+        end: new Date(e.end.dateTime || e.end.date),
+        title: e.summary || 'No title',
+        content: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="size-4">\n' +
+          '  <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />\n' +
+          '</svg>\n',
+        contentFull: e.description || '',
+        backgroundColor: 'pink',
+        color: '#523629',
+        id: e.id,
+        isAllDay: false
+      }
+    }).filter((elem: Record<string, any>) => elem.start && elem.end)
+
+  }
+
+  const updateUserCalendarEvents = (event: CalendarEventType) => {
+    userCalendarEvents.value?.push(event);
+  }
 
   return {
     user,
@@ -140,6 +171,7 @@ export const useUserStore = defineStore('user', () => {
     checkUserSession,
     googleCalendarEvents,
     addNewEventToCalendar,
-    removeGoogleCalendarEvent
+    removeGoogleCalendarEvent,
+    updateUserCalendarEvents
   }
 })
--- src/pages/mama/ui/CalendarBeauty.vue
+++ src/pages/mama/ui/CalendarBeauty.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import {useI18n} from "vue-i18n";
-import {onMounted, watch, computed, ref} from "vue";
+import {onMounted, watch, computed, ref, defineModel} from "vue";
 import { useUserStore } from "@/entities/user";
 import {AppButton} from "@/shared/ui/button";
-const { googleCalendarEvents, user, addNewEventToCalendar, removeGoogleCalendarEvent } = useUserStore();
+const { googleCalendarEvents, user, removeGoogleCalendarEvent } = useUserStore();
 const { userCalendarEvents, isLoading } = storeToRefs((useUserStore()));
 import { useRoute } from 'vue-router'
 const route = useRoute();
@@ -12,11 +12,10 @@ import { VueCal } from 'vue-cal';
 import 'vue-cal/style'
 import {storeToRefs} from "pinia";
 import ModalComponent from "@/shared/ui/modal";
-import {AppTextarea} from "@/shared/ui/form";
-import { v4 as uuidv4 } from 'uuid';
 import type { CalendarEventType } from "@/entities/user";
 import { parseDateToString } from "@/shared/lib/parseDateToString.ts";
 import LoaderComponent from "@/features/loader";
+import { AddEditEventForm } from "@/entities/event";
 
 const connectGoogleCalendar = () => {
   window.location.href = 'http://localhost:3000/user/google/check';
@@ -26,37 +25,12 @@ type FormEventType = {
   description: string;
 }
 
-const calendarEventsList = ref<Record<string, any>[] | null>(null);
 const isDialogOpen = ref<boolean>(false);
 const formEventData = ref<FormEventType>({
   title: '',
   description: ''
 });
 
-const getGoogleCalendarEvents = async () => {
-  await googleCalendarEvents('beauty');
-
-  console.log('result', userCalendarEvents.value);
-
-  calendarEventsList.value = userCalendarEvents?.value?.map((e: Record<string, any>) => {
-
-    return {
-      start: new Date(e.start.dateTime || e.start.date),
-      end: new Date(e.end.dateTime || e.end.date),
-      title: e.summary || 'No title',
-      content: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="size-4">\n' +
-        '  <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />\n' +
-        '</svg>\n',
-      contentFull: e.description || '',
-      backgroundColor: 'pink',
-      color: '#523629',
-      id: e.id,
-      isAllDay: false
-    }
-  }).filter((elem: Record<string, any>) => elem.start && elem.end)
-
-  console.log('calendarEventsList.value', calendarEventsList.value)
-}
 
 watch(() => route, (value) => {
   console.log(value)
@@ -64,7 +38,7 @@ watch(() => route, (value) => {
 
 onMounted( async () => {
   if (user?.google_refresh) {
-    await getGoogleCalendarEvents();
+    await googleCalendarEvents('beauty');
   }
 })
 
@@ -73,7 +47,6 @@ type PendingValueType = {
   resolve: ( event: CalendarEventType ) => {}
 }
 const pendingEvent = ref<PendingValueType | null>(null);
-const newEvent = ref<CalendarEventType | null>(null);
 const createEvent = ( { event, resolve }: PendingValueType) => {
   openDialog()
   if (event.start) {
@@ -83,65 +56,9 @@ const createEvent = ( { event, resolve }: PendingValueType) => {
 }
 const vuecalRef = ref<InstanceType<typeof VueCal> | null>(null)
 const message = ref<string>('');
-const saveEventDescription = async () => {
-  const { title, description } = formEventData.value;
-
-  if (title.trim() && description.trim() && pendingEvent.value?.event.start ) {
-
-    const finalizedEvent = {
-      ...pendingEvent.value.event,
-      id: uuidv4(),
-      title: formEventData.value.title,
-      contentFull: formEventData.value.description,
-    };
-
-    pendingEvent.value?.resolve(finalizedEvent);
-
-    const result = await addNewEventToCalendar({
-      body: finalizedEvent,
-      type: 'beauty'
-    });
-
-    if (!result.result) return;
-
-    calendarEventsList.value?.push({
-      start: new Date(result.data.start.dateTime),
-      end: new Date(result.data.end.dateTime),
-      title: result.data.summary || 'No title',
-      content: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="red" class="size-4">\n' +
-        '  <path d="M2 6.342a3.375 3.375 0 0 1 6-2.088 3.375 3.375 0 0 1 5.997 2.26c-.063 2.134-1.618 3.76-2.955 4.784a14.437 14.437 0 0 1-2.676 1.61c-.02.01-.038.017-.05.022l-.014.006-.004.002h-.002a.75.75 0 0 1-.592.001h-.002l-.004-.003-.015-.006a5.528 5.528 0 0 1-.232-.107 14.395 14.395 0 0 1-2.535-1.557C3.564 10.22 1.999 8.558 1.999 6.38L2 6.342Z" />\n' +
-        '</svg>\n',
-      contentFull: result.data.description || '',
-      backgroundColor: 'pink',
-      color: '#523629',
-      id: result.data.id,
-      isAllDay: false
-    });
-
-    console.log(calendarEventsList?.value)
-    closeModal();
-    return;
-  }
-  message.value = 'Can not save empty fields';
-
-}
-
 const openDialog = () => {
   isDialogOpen.value = true;
 }
-
-const closeModal = () => {
-  console.log('calendarEventsList.value --- ', calendarEventsList.value)
-  formEventData.value = {
-    title: '',
-    description: ''
-  }
-
-  newEvent.value = null;
-  isDialogOpen.value = false;
-  pendingEvent.value = null;
-}
-
 
 const isDetails = ref<boolean>(false);
 const currentEvent = ref<CalendarEventType | null>(null);
@@ -161,12 +78,29 @@ const deleteEvent = async (id: string) => {
   vuecalRef.value?.view.deleteEvent({ id }, 3);
   isDetails.value = false;
 }
+const editEvent = async (id: string) => {
+  isDialogOpen.value = true;
+  isDetails.value = false;
+}
 
+const dropEvent = ({ event }: { event: CalendarEventType}) => {
+  console.log(event)
+}
 
+const resetForm = () => {
+  isDialogOpen.value = false;
+  pendingEvent.value = null;
+  formEventData.value = {
+    title: '',
+    description: ''
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-col justify-start items-start h-full w-full overflow-hidden p-5 bg-gradient-main relative">
+    <code>{{ formEventData }}</code>
+    <code>{{ isDialogOpen }}</code>
     <div v-if="!isLoading"  class="w-full">
       <h2 class="text-brown-dark font-semibold mb-4 text-xl">{{ t('mama.beauty_calendar') }}:</h2>
       <div class="text-brown-dark flex flex-col justify-between items-start gap-3 mb-6">
@@ -191,10 +125,10 @@ const deleteEvent = async (id: string) => {
         :disable-views="[]"
         :editable-events="{ create: true, resize: false,  drag: true, delete: true }"
         :min-event-width="0"
-        v-model:events="calendarEventsList"
+        v-model:events="userCalendarEvents"
         @event-create="createEvent"
         @event-click="showDetails"
-
+        @event-drop = "dropEvent"
       />
     </div>
     <LoaderComponent v-else />
@@ -202,46 +136,22 @@ const deleteEvent = async (id: string) => {
 
 
   <!--  dialog add event -->
-  <ModalComponent v-if="!isLoading" :is-show="isDialogOpen" full>
+  <ModalComponent v-if="!isLoading && isDialogOpen" full>
     <template #content>
-      <div class="bg-white p-5 rounded-md w-2/6 h-auto flex flex-col items-start justify-start gap-4">
-        <h2 class="self-center">{{ t('mama.event.modal_title') }}</h2>
-        <form action="" class="flex flex-col items-start justify-start gap-4 w-full">
-          <div class="w-full">
-            <h2 class="mb-2">{{ t('mama.event.modal_event_name') }}</h2>
-            <input
-              v-model="formEventData.title"
-              type="text"
-              class="w-full dark-mode placeholder:opacity-25"
-              :placeholder="t('mama.event.enter_event_name')"
-            />
-          </div>
-          <div class="w-full">
-            <h2 class="mb-2">{{ t('mama.event.modal_event_description') }}</h2>
-            <AppTextarea
-              v-model="formEventData.description"
-              :message="message"
-              class="w-full"
-              dark-mode
-              placeholder-text="mama.event.enter_event_description"
-            />
-          </div>
-        </form>
-        <div class="flex justify-start items-center gap-4">
-          <AppButton :label="t('general.close')" @click="closeModal" />
-          <AppButton :label="t('general.save')" @click="saveEventDescription" />
-        </div>
-
-      </div>
+      <AddEditEventForm
+        v-model="formEventData"
+        :pending-event="pendingEvent"
+        :reset-form="resetForm"
+      />
     </template>
   </ModalComponent>
 
   <!--  dialog show details -->
-  <ModalComponent v-if="!isLoading" :is-show="isDetails" full>
+  <ModalComponent v-if="!isLoading && isDetails"  full>
     <template #content>
       <div v-if="currentEvent" class="bg-white text-brown-dark p-5 rounded-md w-2/6 h-auto flex flex-col items-start justify-start gap-4">
         <h2 class="self-center font-bold text-xl w-full p-2 text-center">{{ t('mama.event.modal_title') }}</h2>
-        <div class="mb-2">
+        <div class="mb-4">
           <p>
             <span class="font-bold">{{ t('mama.event.title') }}</span>:
             {{ currentEvent.title }}
@@ -259,9 +169,10 @@ const deleteEvent = async (id: string) => {
             {{ currentEvent.end }}
           </p>
         </div>
-        <div class="flex justify-start items-center gap-4">
+        <div class="flex justify-between items-center gap-2 w-full">
           <AppButton :label="t('general.close')" @click="isDetails = false" />
           <AppButton :label="t('general.delete')" @click="deleteEvent(currentEvent.id)" />
+          <AppButton :label="t('general.edit')" @click="editEvent(currentEvent.id)" />
         </div>
       </div>
       <div v-else >
