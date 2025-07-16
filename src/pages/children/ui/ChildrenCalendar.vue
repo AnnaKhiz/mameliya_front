3fd216169887920bab6c48e8c58eacd3fbc6bfba
annakhizhniak <kukurika.anna@gmail.com>
2025-07-16 10:00:18 +0300

    [frontend] added layout to the children calendar

--- src/pages/children/ui/ChildrenCalendar.vue
+++ src/pages/children/ui/ChildrenCalendar.vue
@@ -1,9 +1,63 @@
 <script setup lang="ts">
+import {useI18n} from "vue-i18n";
+import { ref } from "vue";
+import {
+  type DialogEventsType,
+  CalendarComponent,
+} from "@/entities/calendar";
+const { t } = useI18n();
 
+import ModalComponent from "@/shared/ui/modal";
+import FAQCalendarInfo from "@/pages/faq/ui/FAQCalendarInfo.vue";
+import {AppButton} from "@/shared/ui/button";
+
+const dialog = ref<DialogEventsType>('none');
+const changeDialogState = (value: DialogEventsType) => {
+  dialog.value = value;
+}
 </script>
 
 <template>
-<div>children calendar</div>
+
+    <div class="w-full grow-1">
+      <div class="flex justify-between items-start">
+        <h2 class="text-brown-dark font-semibold mb-4 text-xl">{{ t('aside.children.daily_routine') }}:</h2>
+        <p
+          class="cursor-pointer font-semibold underline hover:text-brown-medium"
+          @click="changeDialogState('instruction')"
+        >
+          {{ t('mama.how_to_use') }}
+        </p>
+      </div>
+
+      <CalendarComponent :type="'children'"  />
+    </div>
+
+
+  <!-- dialog instruction   -->
+  <ModalComponent
+    :show="dialog === 'instruction'"
+    full
+    :title="t('mama.how_to_use')"
+    :width="'w-4/6'"
+    @update:dialog-visibility="changeDialogState"
+  >
+    <section class="w-full">
+      <FAQCalendarInfo
+        @update:dialog-visibility="changeDialogState"
+      >
+        <template #content>
+          <div class="flex flex-col justify-between items-start gap-2 mb-6">
+            <p>{{ t('family.family_calendar_about') }}</p>
+            <p>{{ t('family.family_calendar_what_inside') }}</p>
+          </div>
+        </template>
+      </FAQCalendarInfo>
+    </section>
+    <div class="flex justify-start items-center gap-2 w-full">
+      <AppButton :label="t('general.close')" @click="changeDialogState" />
+    </div>
+  </ModalComponent>
 </template>
 
 <style scoped>
