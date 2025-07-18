7832b3bb83d1b2b0b3c2b31bfc381cf45f40da2f
annakhizhniak <kukurika.anna@gmail.com>
2025-07-18 14:27:29 +0300

    [frontend] added translations for ritualDetailsActionsMenu

--- src/entities/ritual/ui/RitualDetailsActionsMenu.vue
+++ src/entities/ritual/ui/RitualDetailsActionsMenu.vue
@@ -7,9 +7,20 @@ import {
 } from "@heroicons/vue/16/solid";
 import { useRitualStore } from "@/entities/ritual/model/useRitualStore.ts";
 import {storeToRefs} from "pinia";
-const { openAddRitualForm, toggleIsChecked, toggleIsCheckedMultiple, saveToMyRituals } = useRitualStore();
-const { isChecked, anyChecked } = storeToRefs(useRitualStore());
-
+import {useI18n} from "vue-i18n";
+const {
+  openAddRitualForm,
+  toggleIsChecked,
+  toggleIsCheckedMultiple,
+  saveToMyRituals
+} = useRitualStore();
+const {
+  isChecked,
+  anyChecked,
+  allChecked,
+  isAddNewForm,
+} = storeToRefs(useRitualStore());
+const { t } = useI18n();
 type Props = {
   isAddIcon: boolean;
 }
@@ -22,25 +33,25 @@ defineProps<Props>();
       v-if="isAddIcon"
       class="w-8 p-1 outline-none fill-brown-medium hover:fill-brown-dark hover:bg-brown-light/40 hover:rounded hover:cursor-pointer transition duration-500"
       @click="openAddRitualForm"
-      v-tooltip="'Add new ritual'"
+      v-tooltip="isAddNewForm ? t('rituals.hide_add_new_ritual') : t('rituals.add_new_title')"
     />
     <div class="mb-2 flex items-center justify-end gap-1">
       <ChevronDoubleUpIcon
         @click="toggleIsChecked"
         :class="isChecked ? 'rotate-180' : ''"
         class="w-8 p-1 outline-none fill-brown-medium hover:fill-brown-dark hover:rounded hover:cursor-pointer hover:bg-brown-light/40 transition duration-500"
-        v-tooltip="'Show checkboxes'"
+        v-tooltip="isChecked ? t('rituals.hide_checkboxes') :  t('rituals.show_checkboxes')"
       />
       <CheckCircleIcon
         class="w-8 p-1 outline-none fill-brown-medium hover:fill-brown-dark hover:bg-brown-light/40 hover:rounded hover:cursor-pointer transition duration-500"
         @click="toggleIsCheckedMultiple"
-        v-tooltip="'Check all'"
+        v-tooltip="allChecked ? t('rituals.uncheck_all') : t('rituals.check_all')"
       />
       <StarIcon
         v-if="anyChecked"
         class="w-8 p-1 outline-none fill-brown-medium hover:fill-brown-dark hover:bg-brown-light/40 hover:rounded hover:cursor-pointer transition duration-500"
         @click="saveToMyRituals"
-        v-tooltip="'Add to My rituals'"
+        v-tooltip="t('rituals.add_to_my_rituals')"
       />
     </div>
   </div>
