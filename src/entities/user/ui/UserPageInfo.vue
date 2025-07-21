13e032c590ad7b9adfe648c425e8446224032c54
annakhizhniak <kukurika.anna@gmail.com>
2025-07-21 13:07:10 +0300

    [frontend] added render user data from the store

--- src/entities/user/ui/UserPageInfo.vue
+++ src/entities/user/ui/UserPageInfo.vue
@@ -3,6 +3,9 @@
 import {AppButton} from "@/shared/ui/button";
 import {useI18n} from "vue-i18n";
 import {ListItem} from "@/shared/ui/list-item";
+import { useUserStore } from "@/entities/user";
+import {storeToRefs} from "pinia";
+const { user } = storeToRefs(useUserStore());
 const { t } = useI18n();
 
 const emits = defineEmits(['update:edit']);
@@ -12,26 +15,23 @@ const emits = defineEmits(['update:edit']);
   <div class="w-full h-full flex flex-col gap-4 text-brown-dark">
     <ListItem
       :title="t('user_page.user_name')"
-      :content="'Name'"
+      :content="user?.first_name ?? 'No name'"
     />
     <ListItem
       :title="t('user_page.user_lastname')"
-      :content="'Lastname'"
+      :content="user?.last_name ?? 'No name'"
     />
     <ListItem
       :title="t('user_page.user_age')"
-      :content="'Age'"
+      :content="user?.age ?? 'No age'"
     />
     <ListItem
       :title="t('user_page.user_email')"
-      :content="'Email'"
-    />
-    <ListItem
-      :title="t('user_page.password')"
-      :content="'Password'"
+      :content="user?.email"
     />
     <AppButton class="mt-auto mb-5" :label="t('general.edit')" @click.prevent="emits('update:edit', true)" />
   </div>
+
 </template>
 
 <style scoped>
